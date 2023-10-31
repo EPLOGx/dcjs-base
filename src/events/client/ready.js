80864f85e458d154eca2bc9df4cb18dc9e4e5dba
EPLOG <85833642+EPLOGx@users.noreply.github.com>
2023-10-31 21:51:47 +0100

    Add files via upload

---
+++ src/events/client/ready.js
@@ -0,0 +1,10 @@
+const { ActivityType } = require('discord.js')
+
+module.exports = {
+  customname: "on_ready",
+  name: "ready",
+  once: true,
+  async execute(client) {
+    // your code
+  }
+};
